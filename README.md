3ef21bd2d32f8714cf56534782982697549731bd
Michele <misto89@hotmail.it>
2013-02-16 00:28:38 +0100

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 Table-plugin
 ============
 
-A simple plugin for creating pretty tables
+This is a simple plugin that can be used for creating pretty tables. 
+
+This uses technologies like:
+- HTML5 (require html5 doctype declaration);
+- CSS3;
+- JQuery (the version used is the 1.8.1). The plugin has been tested with 1.7 and 1.8 versions.
---
+++ js/style.js
@@ -0,0 +1,44 @@
+$('document').ready(function() {
+	
+	//For .table-hover class--------------------------------------------------------------------------------
+	
+	/*
+	 * on function (or delegate function in the previous jquery version) allows to dynamically 
+	 * assign event handler to the present and future nodes.
+	 */
+	$('.table-hover').on('hover', 'td', function(event) {
+		
+		$(this).parent().toggleClass('hover'); //add or remove class to the current row
+		
+		var index = $(this).index(); //index of the current column
+		$('.table-hover col').eq(index).toggleClass('hover'); //add or remove class to the current column
+		
+	});
+	
+	
+	//For .table-disappearance class-------------------------------------------------------------------------
+	
+	var EXPAND_TEXT = "input <small>(click to expand)</small>";
+	var REDUCE_TEXT = "input <small>(click to reduce)</small>";
+	var KEY = "input" //the key for select all th contains input word
+	
+	var firstRow = $('.table-disappearance tbody th:first').parent();
+	var titleRow = $('<tr><th colspan="5">' + EXPAND_TEXT + '</th></tr>');
+	
+	titleRow
+		.addClass('group')
+		.insertBefore(firstRow)
+		.click(function() {
+			if ( $(this).hasClass('open')) {
+				$(this).siblings(':contains(' + KEY + ')').fadeOut('normal');
+				$(this).find('th').html(EXPAND_TEXT);
+			} else {
+				$(this).siblings(':contains(' + KEY + ')').fadeIn('normal');
+				$(this).find('th').html(REDUCE_TEXT);
+			}
+			
+			$(this).toggleClass('open');
+		})
+		.siblings(':contains(' + KEY + ')') //or .siblings().slice(0, n) for select n siblings
+		.fadeOut('fast');
+});
